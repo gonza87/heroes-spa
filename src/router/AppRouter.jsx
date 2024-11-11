6622f5c801b89fa5968e8293d0f0a00f85c9ea86
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-11-11 11:51:51 -0300

    cambios

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import MarvelPage from "../heroes/pages/MarvelPage";
+import DcPages from "../heroes/pages/DcPages";
+import LoginPage from "../auth/pages/LoginPage";
 
 function AppRouter() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<About />} />
+        <Route path="/marvel" element={<MarvelPage />} />
+        <Route path="/dc" element={<DcPages />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
   );
