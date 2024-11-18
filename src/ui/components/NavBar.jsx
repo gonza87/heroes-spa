822ac7e5601136c32fbba22a4844ed659420a3f3
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-11-18 10:24:37 -0300

    nav

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,6 +1,10 @@
 import { Link, NavLink } from "react-router-dom";
 
 export const NavBar = () => {
+  const handleLogout = () => {
+    console.log("logout");
+  };
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <Link className="navbar-brand" to="/">
@@ -25,9 +29,9 @@ export const NavBar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
         <ul className="navbar-nav ml-auto">
-          <NavLink className="nav-item nav-link" to="/login">
+          <button onClick={handleLogout} className="nav-item nav-link">
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
